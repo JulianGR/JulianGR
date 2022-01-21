2bae30ba9b351b14775cb906b6b6970ae6f7e4b8
Julián Gómez <juliangr505@gmail.com>
2022-01-21 13:44:51 +0100

    Update parrot.sh

--- init-scripts/parrot.sh
+++ init-scripts/parrot.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt install -y virtualbox-guest-* appstream apt-config-icons apt-file	apt-transport-https binutils-mingw-w64-x86-64 bloodhound cgroupfs-mount cherrytree code default-mysql-client ffuf fonts-open-sans ftp fwupd	fwupd-amd64-signed gnome-software gnome-software-common hexedit kate mingw-w64-common mingw-w64-x86-64-dev neo4j nfs-common  nomacs npm openvpn pngcheck rlwrap rpcbind runc seclists software-properties-common software-properties-gtk ssss volatility3 openjdk-14-jdk openjdk-14-jre fonts-powerline flameshot openvpn-systemd-resolved git wget curl python3 python3-pip openjdk-8-jre vlc  kate p7zip-full nodejs npm build-essential python3-pip python3-dev python3-setuptools ca-certificates curl gnupg lsb-release
+sudo apt install -y virtualbox-guest-* appstream apt-config-icons apt-file apt-transport-https binutils-mingw-w64-x86-64 bloodhound cgroupfs-mount cherrytree code default-mysql-client ffuf fonts-open-sans ftp fwupd	fwupd-amd64-signed gnome-software gnome-software-common hexedit kate mingw-w64-common mingw-w64-x86-64-dev neo4j nfs-common  nomacs npm openvpn pngcheck rlwrap rpcbind runc seclists software-properties-common software-properties-gtk ssss volatility3 openjdk-14-jdk openjdk-14-jre fonts-powerline flameshot openvpn-systemd-resolved git wget curl python3 python3-pip openjdk-8-jre vlc  kate p7zip-full nodejs npm build-essential python3-pip python3-dev python3-setuptools ca-certificates curl gnupg lsb-release
 
 # google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
